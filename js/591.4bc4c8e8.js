0cc3658763327a115ae7e8194013fd057a4ab856
Stefan Fenn <stefan.fenn@smart-cybersecurity.de>
2024-05-16 00:12:08 +0200

    Updates

---
+++ js/591.4bc4c8e8.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvite_vue_starter"]=self["webpackChunkvite_vue_starter"]||[]).push([[591],{591:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=591.4bc4c8e8.js.map
